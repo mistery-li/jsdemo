c3946db00af91a153e1cf6d47df29fbee61dc0bf
jin <litujin1123@gmail.com>
2017-07-27 03:09:44 +0800

    gengxin

--- demo/demo.js
+++ demo/demo.js
@@ -4,23 +4,28 @@ var positions = ['0 -854', '-174 -852', '-349 -852', '-524 -852', '698 \
 
 var ele = document.getElementById('rabbit');
 
-animation(ele, positions, imgUrl);
+var animation = window.animation;
 
-function animation(ele, positions, imgUrl) {
+var repeatAnimation = animation().loadImage([imgUrl]).changePosition(ele, positions,imgUrl).repeatForever();
+repeatAnimation.start(80);
+
+// animation(ele, positions, imgUrl);
+
+// function animation(ele, positions, imgUrl) {
     
-    ele.style.backgroundImage = 'url(' + imgUrl + ')';
-    ele.style.backgroundRepeat = 'no-repeat';
+//     ele.style.backgroundImage = 'url(' + imgUrl + ')';
+//     ele.style.backgroundRepeat = 'no-repeat';
 
-    var index = 0;
+//     var index = 0;
 
-    function run() {
-        var position = positions[index].split(' ');
-        ele.style.backgroundPosition = position[0] + 'px' + position[1] + 'px';
-        index++;
-        if (index >= positions.length) {
-            index = 0;
-        }
-        setTimeout(run, 80);
-    }
-    run();
-}
+//     function run() {
+//         var position = positions[index].split(' ');
+//         ele.style.backgroundPosition = position[0] + 'px' + position[1] + 'px';
+//         index++;
+//         if (index >= positions.length) {
+//             index = 0;
+//         }
+//         setTimeout(run, 80);
+//     }
+//     run();
+// }
